b70fee94cc1624b6db3a0f127885f56111a53300
Max Meza <matt1007k@gmail.com>
2024-07-21 22:07:43 -0500

    Initial Commit

---
+++ SwittUICatalog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwittUICatalog
+//
+//  Created by Max Meza on 7/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwittUICatalog/SwittUICatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwittUICatalogApp.swift
+//  SwittUICatalog
+//
+//  Created by Max Meza on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SwittUICatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
